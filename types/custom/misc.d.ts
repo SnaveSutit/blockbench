8f58efe5b03a877c7ef324cb2d871624f599298b
SnaveSutit <snavesutit@gmail.com>
2026-01-07 14:35:23 -0500

    ✨ Improve type format
    - Renamed `EventName` -> `BlockbenchEventName`
    - Renamed `EventMap` -> `BlockbenchEventMap`
    (Renamed for clarity)
    
    Since events can now be added by users using `interface` merging, I've removed the generic `string` type from event names.

--- types/custom/misc.d.ts
+++ types/custom/misc.d.ts
@@ -20,7 +20,7 @@ declare const isApp: boolean
 
 declare const VuePrismEditor: Vue.Component
 
-interface EventMap {
+interface BlockbenchEventMap {
 	remove_animation: any
 	display_animation_frame: any
 	display_default_pose: any
@@ -131,8 +131,7 @@ interface EventMap {
 	edit_collection_properties: any
 }
 
-type EventName = keyof EventMap
-type EventData<T extends string = EventName> = T extends keyof EventMap ? EventMap[T] : any
+type BlockbenchEventName = keyof BlockbenchEventMap
 
 type IconString = string
 
